a69df6aaaf8c7e38051d59af74a223f49df3bec8
Tania <taniadiasr@gmail.com>
2015-11-12 00:10:43 -0200

    arduino beta code add

---
+++ arduino/readsensor/readsensor.ino
@@ -0,0 +1,21 @@
+/***************************************\
+* TaniaR :: Date 11.11.2015 - 1.0.0 *** 
+* Temperature Logger Reader ***********
+* Development board: Arduino UNO ******
+* Sensor NTC + 10k Resistor ***********
+\**************************************/
+
+const int pinSensor = 0; //analog input pin 0
+int valueSensor = 0;
+float temp;
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  valueSensor = analogRead(pinSensor);
+  temp = valueSensor*0.48828125;
+  Serial.println(valueSensor);
+  Serial.println(temp);
+  delay(1000);
+}
